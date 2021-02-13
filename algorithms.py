28bc719de8a53594a9c94ee630c3fdc954377f4d
redwrasse <redwrasse@users.noreply.github.com>
2021-02-12 23:57:56 -0800

    alg62 backprop, works on tree with binary addition

--- algorithms.py
+++ algorithms.py
@@ -19,9 +19,14 @@ class SimpleCGraph:
 class SimpleNode:
     """ Operates with function f on parent node values
     to hold an evaluated value val """
-    def __init__(self, f, val=None):
+
+    def __init__(self, f, pd, val=None):
         self.f = f
+        self.pd = pd
         self.val = val
+
+    def partial_derivative(self, child_index, cx):
+        return self.pd(cx)[child_index]
 
 
 def alg61(cgraph, x):
@@ -39,7 +44,30 @@ def alg61(cgraph, x):
     return cgraph.nodes[-1].val
 
 
-def test_alg61():
+def alg62(cgraph, x):
+    """ back-propagation
+    """
+    alg61(cgraph, x)  # forward prop., setting vals at nodes.
+    n = len(cgraph.nodes)
+    # partial derivatives of all nodes wrt. root
+    # grad_table[i] = del root del v_i
+    grad_table = [0.] * n
+    grad_table[n-1] = 1.
+    for j in range(n-2, -1, -1):
+        # would be faster with adj list given, not just reverse adj list
+        child_indices = []
+        grad_table_j = 0.
+        for i in cgraph.reverse_adj:
+            if j in cgraph.reverse_adj[i]:
+                child_indices.append(i)
+                parent_vals = [cgraph.nodes[ix].val for ix in cgraph.reverse_adj[i]]
+                pd_ix = cgraph.reverse_adj[i].index(j)
+                grad_table_j += grad_table[i] * cgraph.nodes[i].partial_derivative(pd_ix, parent_vals)
+        grad_table[j] = grad_table_j
+    return grad_table
+
+
+def test():
     """
          5
         /\
@@ -48,6 +76,7 @@ def test_alg61():
     0  1  2
     """
     def f(arr): return sum(arr)
+    def pd(arr): return [1, 1]
     n = 6
     x = [0.5, 0.1, 0.3]
     reverse_adj = {
@@ -55,9 +84,13 @@ def test_alg61():
         3: [0, 1],
         4: [2]
     }
-    nodes = [SimpleNode(f, None) for _ in range(n)]
+    nodes = [SimpleNode(f, pd, None) for _ in range(n)]
     cgraph = SimpleCGraph(reverse_adj, nodes)
     root_val = alg61(cgraph, x)
-    print(root_val)
+    print(f"root node value: {root_val}")
 
-test_alg61()
+    grad_table = alg62(cgraph, x)
+    print(f"grad table: {grad_table}")
+
+
+test()
