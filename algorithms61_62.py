647b9003b962f6785943827c0c4d674275fd9f55
redwrasse <redwrasse@users.noreply.github.com>
2021-02-15 16:34:01 -0800

    reverse adj graph for mlp

--- algorithms61_62.py
+++ algorithms61_62.py
@@ -112,24 +112,61 @@ def sample_graph_structure():
     return reverse_adj
 
 
-def mlp_graph_structure():
+def mlp_graph_structure(n, k):
     """
     fully connected mlp graph structure
-    half the parents of any node the corresponding
+    use convention of first half of the parents of any node the corresponding
     weight parameters,
-    the other half the previous layer outputs.
+    the second half the previous layer outputs.
 
-    k layers each connecting n nodes to n nodes,
-    plus final layer is 1 node only (alg61 and 62 as
+    mlp output is 1d (alg61 and 62 as
     currently implemented support 1d root only)
 
-    nodes 0, ... , n-1 first layer
-          n, ...., 2n - 1 second layer, etc.
+    k layers each connecting n nodes to n nodes,
+    plus final layer as 1 node only.
 
-          n(k-1), ..., nk - 1 second to last layer
-          nk root node for last layer
+    So
+
+    nodes 0, ... , n-1 first layer
+                0,....,n/2 - 1 is first layer param weights
+                n/2,..., n-1 is inputs to first layer(=x)
+          n, ..., 2n - 1 second layer, etc.
+                n, ..., 3n/2 - 1 is second layer param weights
+                3n/2, ..., 2n-1 is inputs to second layer
+          n(k-1), ..., nk - 1 second to last (aka kth) layer
+                (etc)
+          nk root node as final layer
+
+    layer index l = 0, ...., k - 1
+            layer l is nodes nl,...., n(l+1) - 1
+                            nodes nl, ..., n(l + 1/2) - 1 param weights
+                            nodes n(l + 1/2), ... , n(l+1) - 1 is layer inputs
+
     """
-    pass
+    if n % 2 != 0:
+        raise Exception("n must be even.")
+    reverse_adj = {}
+    for l in range(k-1):
+        weights_indices = list(range(n*l, int(n*(l + 0.5))))
+        layer_input_indices = list(range(int(n*(l + 0.5)), n*(l+1)))
+        for n1 in weights_indices + layer_input_indices:
+            if n1 not in reverse_adj:
+                reverse_adj[n1] = []
+
+        next_layer_inputs = list(range(int(n*(l+1 + 0.5)), int(n*((l+2)))))
+        for n2 in next_layer_inputs:
+            if n2 not in reverse_adj:
+                reverse_adj[n2] = []
+            for parent_n2 in weights_indices + layer_input_indices:
+                reverse_adj[n2].append(parent_n2)
+
+    # final root node layer
+    if n*k not in reverse_adj:
+        reverse_adj[n*k] = []
+    for parent_n in range(n*(k-1), n*k):
+        reverse_adj[n*k].append(parent_n)
+
+    return reverse_adj
 
 
 def example1():
@@ -180,10 +217,12 @@ def example2():
 
 
 def example3():
-    pass
+    n, k = 4, 2
+    print(mlp_graph_structure(n, k))
 
 
 if __name__ == "__main__":
-    example1()
-    print("----------")
-    example2()
+    # example1()
+    # print("----------")
+    # example2()
+    example3()
