0eee9c449db57f556aa468766944ec7ca14543a9
redwrasse <redwrasse@users.noreply.github.com>
2021-02-15 18:25:43 -0800

    working (most likely) mlp minibatch backpropagation

--- algorithms61_62.py
+++ algorithms61_62.py
@@ -96,6 +96,43 @@ def run_backprop_algorithm(cgraph, x, param_indices,
             print(f'loss: {cgraph.nodes[-1].val}')
 
 
+def run_backprop_algorithm_batches(cgraph, xb, x, param_indices,
+                                   data_indices,
+                                   batch_size=10,
+                                   learning_rate=1e-3,
+                                   n_iterations=10**5,
+                                   print_freq=10**2):
+
+    import random
+
+    ns = len(xb)
+    for i in range(n_iterations):
+        random.shuffle(xb)
+        for j in range(0, ns-batch_size, batch_size):
+            xb_minibatch = xb[j:j + batch_size]
+            del_ix = {}
+            for ix in param_indices:
+                del_ix[ix] = 0.
+
+            for xb_e in xb_minibatch:
+                for m, xdata_ix in enumerate(data_indices[:-1]):
+                    x[xdata_ix] = xb_e[0][m]  # x values
+                x[data_indices[-1]] = xb_e[1]  # y value
+                alg61(cgraph, x)
+                grad_table = alg62(cgraph, x)
+                for ix in param_indices:
+                    param_update = -learning_rate * grad_table[ix]
+                    del_ix[ix] += param_update
+
+            # mini-batch param update
+            for ix in param_indices:
+                x[ix] += del_ix[ix] / batch_size
+
+        if i % print_freq == 0:
+            print(f'param values: {[x[_] for _ in param_indices]}')
+            print(f'loss: {cgraph.nodes[-1].val}')
+
+
 def sample_graph_structure():
     """
             5
@@ -135,12 +172,19 @@ def mlp_graph_structure(n, k):
                 3n/2, ..., 2n-1 is inputs to second layer
           n(k-1), ..., nk - 1 second to last (aka kth) layer
                 (etc)
-          nk root node as final layer
+          nk node as final layer = output of mlp f(x)
 
     layer index l = 0, ...., k - 1
             layer l is nodes nl,...., n(l+1) - 1
                             nodes nl, ..., n(l + 1/2) - 1 param weights
                             nodes n(l + 1/2), ... , n(l+1) - 1 is layer inputs
+
+
+        Then additional two nodes: output target leaf y, and square difference
+        (y-f(x))^2.
+
+        nk + 1 node = leaf y
+        nk + 2 node = (y - f(x))^2
 
     """
     if n % 2 != 0:
@@ -160,12 +204,15 @@ def mlp_graph_structure(n, k):
             for parent_n2 in weights_indices + layer_input_indices:
                 reverse_adj[n2].append(parent_n2)
 
-    # final root node layer
+    # output node f(x)
     if n*k not in reverse_adj:
         reverse_adj[n*k] = []
     for parent_n in range(n*(k-1), n*k):
         reverse_adj[n*k].append(parent_n)
-
+    # output target node y
+    reverse_adj[n*k + 1] = []
+    # node (y - f(x))^2, w/ parents as y, f(x)
+    reverse_adj[n*k + 2] = [n*k + 1, n*k]
     return reverse_adj
 
 
@@ -216,11 +263,11 @@ def example2():
     run_backprop_algorithm(cg2, x, param_indices=[0, 1])
 
 
-def example3():
+def mlp_example():
     """
     mlp
     """
-    n, k = 4, 2
+    n, k = 6, 2
     reverse_adj = mlp_graph_structure(n, k)
 
     import random
@@ -235,32 +282,58 @@ def example3():
                 dotp += arr[i] * arr[i + m]
             return dotp
 
+        # for (y-f(x))^2
+        def fb(arr): return (arr[0] - sum(arr[1:])) ** 2
+        def pdb(arr):
+            return [2 * (arr[0] - sum(arr[1:]))] + \
+                   [-2 * (arr[0] - sum(arr[1:]))] * (len(arr) - 1)
+
         def pd(arr):
             # y = <w,x>
             # del y del ni = x_i for first half, w_i for second half
             m = int(len(arr)/2)
             return arr[m:] + arr[:m]
 
-        nodes = [SimpleNode(f, pd, None) for _ in range(n*k + 1)]
+        main_nodes = [SimpleNode(f, pd, None) for _ in range(n*k + 1)]
+        y_node = [SimpleNode(f, pd, None)]  # dummy node for y (leaf) variable
+        sq_loss_node = [SimpleNode(fb, pdb, None)]  # for (y - f(x))^2 node
+        nodes = main_nodes + y_node + sq_loss_node
         cgraph = SimpleCGraph(reverse_adj, nodes)
         return cgraph
 
     cgraph_mlp = cgraph3()
     # all input values plus initial parameter values- aka all leaf values
     # k layers each n / 2 leaf weights,
-    # first layer nas n total leaves because of additional n/2 inputs
-    # => (k-1) * n / 2 + n total # of leaves
-    x_inputs = [random.normalvariate(0., 1.) for _ in range(int(n/1))]
-    x = [random.random() for _ in range(int((k-1) * n/2 + n))]
+    # first layer has n total leaves because of additional n/2 inputs
+    # plus target y as leaf
+    # => (k-1) * n / 2 + n + 1 total # of leaves
+    d = int(n/2)  # dim. of input
+    x_inputs = [random.normalvariate(0., 1.) for _ in range(d)]
+    y_input = 1.
+    x = [random.random() for _ in range(int((k-1) * n/2 + n + 1))]
+    x[-1] = y_input
+    x[d:n] = x_inputs
     # param_indices to train are weights only:
     # nodes nl, ..., n(l + 1/2) - 1 for l = 0,...., k-1
     param_indices = [e for l in range(k) for
                      e in list(range(n*l, int(n*(l + 0.5))))]
-    run_backprop_algorithm(cgraph_mlp, x, param_indices=param_indices)
+
+    ns = 100  # of training points
+    xy_batch = [([random.normalvariate(1., 1.) for dim in range(n)], 1.) for _ \
+                in range(ns//2)] + \
+               [([random.normalvariate(-1., 1.) for dim in range(n)], -1.) for _ \
+                in range(ns // 2)]
+
+    data_indices = list(range(d, n)) + [len(x)-2]  # 2nd to last index = y
+    run_backprop_algorithm_batches(cgraph_mlp,
+                                   xb=xy_batch,
+                                   x=x,
+                                   param_indices=param_indices,
+                                   data_indices=data_indices)
 
 
 if __name__ == "__main__":
     # example1()
     # print("----------")
     # example2()
-    example3()
+    mlp_example()
